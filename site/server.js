2e299705128c1518af2ec67c1bdd7058f005d89f
windhamdavid <david@davidawindham.com>
2014-12-13 08:55:03 -0500

    express upd

--- site/server.js
+++ site/server.js
@@ -5,26 +5,20 @@ var webRTC = require('webrtc.io').listen(server);
 var port = process.env.PORT || 8881;
 server.listen(port);
 
-
-
 app.get('/', function(req, res) {
-  res.sendfile(__dirname + '/index.html');
+  res.sendFile(__dirname + '/index.html');
 });
-
 app.get('/style.css', function(req, res) {
-  res.sendfile(__dirname + '/style.css');
+  res.sendFile(__dirname + '/style.css');
 });
-
 app.get('/fullscrean.png', function(req, res) {
-  res.sendfile(__dirname + '/fullscrean.png');
+  res.sendFile(__dirname + '/fullscrean.png');
 });
-
 app.get('/script.js', function(req, res) {
-  res.sendfile(__dirname + '/script.js');
+  res.sendFile(__dirname + '/script.js');
 });
-
 app.get('/webrtc.io.js', function(req, res) {
-  res.sendfile(__dirname + '/webrtc.io.js');
+  res.sendFile(__dirname + '/webrtc.io.js');
 });
 
 webRTC.rtc.on('chat_msg', function(data, socket) {
